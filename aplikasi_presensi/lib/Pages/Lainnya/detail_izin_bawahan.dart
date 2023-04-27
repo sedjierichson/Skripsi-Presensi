57973596a600b1dbbdc095d4a79a9626e70dfff6
Richson Sedjie <c14190185@john.petra.ac.id>
2023-04-28 00:01:24 +0700

    add info izin dan kehadiran untuk izin pulang lebih awal

--- aplikasi_presensi/lib/Pages/Lainnya/detail_izin_bawahan.dart
+++ aplikasi_presensi/lib/Pages/Lainnya/detail_izin_bawahan.dart
@@ -2,7 +2,10 @@
 
 import 'package:aplikasi_presensi/Pages/Lainnya/izin_bawahan.dart';
 import 'package:aplikasi_presensi/api/dbservices_form_izin.dart';
+import 'package:aplikasi_presensi/api/dbservices_presensi.dart';
 import 'package:aplikasi_presensi/models/izin.dart';
+import 'package:aplikasi_presensi/models/presensi.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -21,6 +24,105 @@ class detailIzinBawahan extends StatefulWidget {
 
 class _detailIzinBawahanState extends State<detailIzinBawahan> {
   FormIzinService db = FormIzinService();
+  PresensiService db2 = PresensiService();
+  String? bulantahunFilter;
+  List<Presensi> kehadiran = [];
+  List<Izin> daftarIzin = [];
+  int jumlahKategoriA = 0;
+  int jumlahKategoriB = 0;
+  int jumlahKategoriC = 0;
+  int jumlahKategoriD = 0;
+  int jumlahPulangLebihAwal = 0;
+  bool isLoadingAll = true;
+  bool isErrorAll = false;
+
+  void getDataPresensi({DateTime? filter}) async {
+    try {
+      kehadiran = await db2.getDataPresensi(
+        nik: widget.detail.nik.toString(),
+      );
+      String tahunFilter = DateFormat('yyyy').format(filter!);
+      String bulanFilter = DateFormat('MM').format(filter);
+      kehadiran.retainWhere((element) => element.tanggal
+          .toString()
+          .contains('$tahunFilter' + '-' + '$bulanFilter'));
+      getJumlahKehadiran();
+      setState(() {
+        isLoadingAll = false;
+      });
+    } catch (e) {
+      setState(() {
+        isLoadingAll = false;
+        isErrorAll = true;
+      });
+    }
+  }
+
+  void getJumlahKehadiran() {
+    setState(() {
+      jumlahKategoriA = 0;
+      jumlahKategoriB = 0;
+      jumlahKategoriC = 0;
+      jumlahKategoriD = 0;
+    });
+    for (int i = 0; i < kehadiran.length; i++) {
+      if (kehadiran[i].kategori.toString() == 'A') {
+        jumlahKategoriA += 1;
+      } else if (kehadiran[i].kategori.toString() == 'B') {
+        jumlahKategoriB += 1;
+      } else if (kehadiran[i].kategori.toString() == 'C') {
+        jumlahKategoriC += 1;
+      } else {
+        jumlahKategoriD += 1;
+      }
+    }
+  }
+
+  void getDaftarIzin({DateTime? filter}) async {
+    setState(() {
+      isLoadingAll = true;
+      isErrorAll = false;
+    });
+    try {
+      daftarIzin = await db.getIzin(nikUser: widget.detail.nik.toString());
+      String tahunFilter = DateFormat('yyyy').format(filter!);
+      String bulanFilter = DateFormat('MM').format(filter);
+      daftarIzin.retainWhere((element) => element.tanggalPengajuan
+          .toString()
+          .contains('$tahunFilter' + '-' + '$bulanFilter'));
+      getJumlahIzin();
+      setState(() {
+        isLoadingAll = false;
+      });
+    } catch (e) {
+      setState(() {
+        isLoadingAll = false;
+        isErrorAll = true;
+      });
+    }
+  }
+
+  void getJumlahIzin() {
+    setState(() {
+      jumlahPulangLebihAwal = 0;
+    });
+    for (int i = 0; i < daftarIzin.length; i++) {
+      if (daftarIzin[i].idJenisIzin.toString() == '1') {
+        jumlahPulangLebihAwal += 1;
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    bulantahunFilter = DateFormat('MMMM yyyy')
+        .format(DateTime.parse(widget.detail.tanggalPengajuan.toString()));
+    getDataPresensi(
+        filter: DateTime.parse(widget.detail.tanggalPengajuan.toString()));
+    getDaftarIzin(
+        filter: DateTime.parse(widget.detail.tanggalPengajuan.toString()));
+    super.initState();
+  }
 
   void terimaTolakIzin(String id, String tanggal, String mode) async {
     try {
@@ -145,9 +247,67 @@ class _detailIzinBawahanState extends State<detailIzinBawahan> {
           ),
         ),
         SizedBox(
-          height: 30,
+          height: 20,
         ),
         widget.detail.status == "1" ? cardButtonTerimaTolak() : SizedBox(),
+        SizedBox(
+          height: 8,
+        ),
+        Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(color: HexColor('FFA133')),
+          child: MaterialButton(
+            onPressed: () {
+              showCupertinoModalPopup(
+                context: context,
+                builder: (context) => CupertinoAlertDialog(
+                  title: Text(
+                      'Riwayat Izin & Rekap Kehadiran Bulan $bulantahunFilter'),
+                  actions: [
+                    CupertinoDialogAction(
+                        isDefaultAction: true,
+                        onPressed: () => Navigator.pop(context),
+                        child: Text('OK')),
+                  ],
+                  content: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Jumlah Izin Pulang Lebih Awal $bulantahunFilter',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      Text('$jumlahPulangLebihAwal izin'),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Kehadiran $bulantahunFilter',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      Text('Kategori A = $jumlahKategoriA'),
+                      Text('Kategori B = $jumlahKategoriB'),
+                      Text('Kategori C = $jumlahKategoriC'),
+                      Text('Kategori D = $jumlahKategoriD'),
+                    ],
+                  ),
+                ),
+              );
+            },
+            child: Text(
+              'Lihat Riwayat & Kehadiran',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        )
       ],
     );
   }
